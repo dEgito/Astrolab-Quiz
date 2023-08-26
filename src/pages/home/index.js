f1de231bb76bb34e7217dce03981d6af0c6908b3
Jorge Lucas <jorge.lucasalm@gmail.com>
2023-08-26 20:50:48 -0300

    feat: adicionar react navigation

---
+++ src/pages/home/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import { useFonts } from 'expo-font';
+
+export default function Home() {
+
+  return (
+    <View
+      style={styles.container}
+    >
+      <View
+        style={styles.header}
+      >
+        <Text style={styles.title}> Aaaa</Text>
+      </View>
+
+      <ImageBackground
+        source={require("../../../assets/bg.png")}
+        style={styles.body}
+      >
+
+      </ImageBackground>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#560526',
+  },
+  header: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: 'yellow',
+    fontSize: 24,
+    fontFamily: 'Bungee'
+  },
+  body: {
+    flex: 6,
+
+    backgroundColor: '#000',
+    borderRadius: 25,
+  }
+});
